095038c3f4e0e3b2e0b53a6cb22556c0574ad048
PabloLaMadrid <pablonicolaslm@gmail.com>
2022-09-29 03:56:50 -0300

    First commit

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import { db } from "../../firebase/firebase";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./dropdown.css";
 
 const DropDown = () => {
   const [state, setState] = useState([]);
-
   // ref a la base de datos
   const companyCollection = collection(db, "companies");
 
@@ -13,12 +14,10 @@ const DropDown = () => {
 
   const getCompanies = async () => {
     const getData = await getDocs(companyCollection);
-    setState(getData.docs.map(el => {el.data()}))
-    console.log(state)
+    setState(getData.docs.map((el) => el.data()));
+    console.log(state);
   };
   // Functions
-
-
 
   // useEffect
   useEffect(() => {
@@ -26,8 +25,16 @@ const DropDown = () => {
   }, []); //eslint-disable-line
 
   return (
-    <div>
-      hola
+    <div className="mainContainer">
+      <h1>Searchbar dropdown</h1>
+      <input type="text" className="form-control" placeholder="Search" />
+      <ul className="list-group">
+        {state.map((el) => (
+          <li type="button" className="list-group-item list-group-item-action">
+            {el.nombre}
+          </li>
+        )).slice(0,20)}
+      </ul>
     </div>
   );
 };
