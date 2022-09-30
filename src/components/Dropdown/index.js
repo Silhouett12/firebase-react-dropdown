eb50df65ca5cfd94e1ca83a232704d8d49f815f6
PabloLaMadrid <pablonicolaslm@gmail.com>
2022-09-30 06:44:40 -0300

    Scroll inifinito 50%, search 50%

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { useState, useEffect, useRef, useCallback } from "react";
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  startAfter,
+  limit,
+  orderBy,
+} from "firebase/firestore";
 import { db } from "../firebase/firebase";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./dropdown.css";
@@ -8,57 +16,52 @@ import "./dropdown.css";
 import { Button, Modal } from "react-bootstrap";
 
 const DropDown = () => {
-
- 
-
-
   // DATABASE INFO
   const [state, setState] = useState([]);
   const [search, setSearch] = useState("");
   const [openModal, setOpenModal] = useState(false);
-  const [hasMore, setHasMore] = useState(false);
   const [pageNumber, setPageNumber] = useState(1);
   const [nombre, setNombre] = useState("");
   const [razón, setRazón] = useState("");
   const [teléfono, setTeléfono] = useState("");
   const [nit, setNit] = useState("");
   const [código, setCódigo] = useState("");
+  const [lastVisible, setLastVisible] = useState(null);
+
+  const observer = useRef();
 
   const getCompanies = async () => {
-    let latestDoc = null;
-    try {
-      const companyCollection = collection(db, "companies")
-        .orderBy("nombre", "asc")
-        .startAfter(latestDoc || 0)
-        .limit(20);
-      const getData = await getDocs(companyCollection).then((el) => {
-        setState(
-          ...state,
-          getData.docs.map((el) => el.data())
-        ).setHasMore(el.docs.length > 0);
-      });
-      setHasMore();
-      latestDoc = getData.docs[getData.docs.length - 1];
-    } catch (error) {
-      return alert("An error occured while gettingCompanies");
-    }
+  
+    const companyCollection = query(
+      collection(db, "companies"),
+      orderBy("razón_social"),
+      startAfter(lastVisible || 0),
+      limit(20)
+    );
+    const getData = await getDocs(companyCollection);
+      if (state.length < 1) {setState(
+      ...state, getData.docs.map((el) => el.data()));
+     
+      }
+      if (state.length < 60){setState((prevState) => ([...prevState.concat(getData.docs.map((el) => el.data()))
+
+     ]))}
+      else {return state}
   };
 
   //SEARCH/PAGINATION
 
-  const observer = useRef();
   const lastName = useCallback(
     (node) => {
-      if (observer.current) return observer.current.disconnect();
+      if(observer.current) observer.current.disconnect()
       observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          setPageNumber((prev) => prev + 1);
+        if (entries[0].isIntersecting && state.length > 1) {
+         setPageNumber(e => e + 1)
         }
       });
       if (node) observer.current.observe(node);
-      console.log(node);
     },
-    [hasMore]
+    [state]
   );
 
   const handleSearch = (e) => {
@@ -72,11 +75,7 @@ const DropDown = () => {
     : state.filter((dato) =>
         dato.nombre.toLowerCase().includes(search.toLocaleLowerCase())
       );
-  
-  
   //FORM HANDLERS / METODO POST A FIREBASE
-
-  
 
   const postCollection = collection(db, "companies");
 
@@ -93,17 +92,15 @@ const DropDown = () => {
     console.log(e);
   };
 
-  
   const handleModal = () => {
     setOpenModal(!openModal);
   };
 
-  // useEffect
+  
 
   useEffect(() => {
     getCompanies();
     console.log("hubo un cambio");
-    console.log(pageNumber)
   }, [pageNumber]); //eslint-disable-line
 
   //RENDERIZADO
@@ -111,52 +108,55 @@ const DropDown = () => {
     <div className="mainContainer">
       <h1>Searchbar dropdown</h1>
       <div className="search-container">
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Search"
-        value={search}
-        onChange={handleSearch}
-      />
-      <div className= {search? "scroll-list" : "scroll-list-hidden"}>
-        <ul className="list-group">
-          <li
-            type="button"
-            className="list-group-item list-group-item-action"
-            id="item-create"
-            onClick={() => handleModal()}
-          >
-            Create
-          </li>
-          {results.map((el, index) => {
-            if (results.length === index + 1) {
-              return (
-                <li
-                  ref={lastName}
-                  type="button"
-                  className="list-group-item list-group-item-action"
-                  key={el.código}
-                >
-                  {el.nombre}
-                </li>
-              );
-            } else {
-              return (
-                <li
-                  type="button"
-                  className="list-group-item list-group-item-action"
-                  key={el.código}
-                >
-                  {el.nombre}
-                </li>
-              );
-            }
-          })}
-        </ul>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search"
+          value={search}
+          onChange={handleSearch}
+        />
+        <div className={"scroll-list"}>
+          <ul className="list-group">
+            <li
+              type="button"
+              className="list-group-item list-group-item-action"
+              id="item-create"
+              onClick={() => handleModal()}
+            >
+              Create
+            </li>
+            {results.map((el, index) => {
+              if (results.length === index + 1) {
+                return (
+                  <li
+                    type="button"
+                    ref={lastName}
+                    className="list-group-item list-group-item-action"
+                    key={el.código}
+                  >
+                    {el.nombre}
+                  </li>
+                );
+              } else {
+                return (
+                  <li
+                    type="button"
+                    className="list-group-item list-group-item-action"
+                    key={el.código}
+                  >
+                    {el.nombre}
+                  </li>
+                );
+              }
+            })}
+            {/* <button onClick={() => moreClicks()}>click</button> */}
+          </ul>
+        </div>
       </div>
-      </div>
-      <Modal show={openModal} onHide={() => handleModal()} className="modal" >
-        <Modal.Header closeButton="modal-header"><h1>Company</h1></Modal.Header>
+      <Modal show={openModal} onHide={() => handleModal()} className="modal">
+        <Modal.Header closeButton="modal-header">
+          <h1>Company</h1>
+        </Modal.Header>
         <form onSubmit={store}>
           <div className="md-form mb-5">
             <input
@@ -166,9 +166,7 @@ const DropDown = () => {
               value={nombre}
               onChange={(e) => setNombre(e.target.value)}
             />
-            <label>
-              Nombre
-            </label>
+            <label>Nombre</label>
           </div>
 
           <div className="md-form mb-5">
@@ -179,9 +177,7 @@ const DropDown = () => {
               value={razón}
               onChange={(e) => setRazón(e.target.value)}
             />
-            <label >
-              Razón social
-            </label>
+            <label>Razón social</label>
           </div>
 
           <div className="md-form mb-5">
@@ -193,9 +189,7 @@ const DropDown = () => {
               onChange={(e) => setNit(e.target.value)}
               placeholder="##-###-####"
             />
-            <label >
-              Número de NIT
-            </label>
+            <label>Número de NIT</label>
           </div>
 
           <div className="md-form mb-5">
@@ -207,9 +201,7 @@ const DropDown = () => {
               onChange={(e) => setTeléfono(e.target.value)}
               placeholder="###-###-####"
             />
-            <label >
-              Telefono
-            </label>
+            <label>Telefono</label>
           </div>
           <div className="md-form mb-5">
             <input
@@ -219,9 +211,7 @@ const DropDown = () => {
               value={código}
               onChange={(e) => setCódigo(e.target.value)}
             />
-            <label >
-              Código
-            </label>
+            <label>Código</label>
           </div>
           <Modal.Footer>
             <Button type="submit">Create</Button>
